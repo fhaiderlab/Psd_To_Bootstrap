e691b6a25ac51fc89d3568612fbff5b52135e3fa
Faruque Haider <101053226+fhaiderlab@users.noreply.github.com>
2022-03-15 19:44:07 +0600

    Add files via upload
    First Commit

---
+++ js/main_oo.js
@@ -0,0 +1,53 @@
+// JavaScript Document
+$(document).ready(function(){
+	
+	//sticky menu
+	$(".js--services-section").waypoint(function(direction){
+        if (direction == "down"){
+            $("nav").addClass("sticky");
+        } else {
+            $("nav").removeClass("sticky");
+        }
+    });
+
+	//Active link
+
+	$("nav ul li a").click(function(){
+		$("nav ul li a").removeClass("active");
+		$(this).addClass("active");
+	});
+	
+	$("nav a img.logo").click(function(){
+		$("nav ul li a").removeClass("active");
+		$("nav ul li:first-child a").addClass("active");
+	});
+	
+	//mixitup (portfolio section)
+	var mixer = mixitup('.container');
+});
+
+// Smooth scroll for IE/ Edge / Safari
+
+$("a").on('click', function(event){
+	if (this.hash ! == ""){
+		event.preventDefault();
+		
+		var hash = this.hash;
+		
+		$('html, body').animate({
+			scrollTop: $(hash).offset()top
+		}, 800, function(){
+			window.location.hash = hash;
+		});
+	}
+	
+});
+
+function openNav() {
+	document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+	document.getElementById("myNav").style.width = "0%";
+}
+
